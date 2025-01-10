bb2792e149ec2b48e044c36d5cf89715df6041b5
ubuntu <ubuntu@ip-10-0-1-17>
2025-01-10 18:05:42 +0000

    waleko__gemini-pro-1.5_3bca__id_161

--- tests/test_octodns_processor_filter.py
+++ tests/test_octodns_processor_filter.py
@@ -196,10 +196,6 @@ class TestNetworkValueFilter(TestCase):
 
     def test_bad_config(self):
         with self.assertRaises(ValueError):
-            filter_private = NetworkValueRejectlistFilter(
-                'rejectlist', set(('string', '42.42.42.42/43'))
-            )
-
     def test_reject(self):
         filter_private = NetworkValueRejectlistFilter(
             'rejectlist', set(('10.0.0.0/8', 'fd00::/8'))
